070428b718ae56b93754730f1085a1d5b010dc76
Gustavo-daCosta <gustavo.costa70@aluno.senai.br>
2023-08-04 17:15:38 -0300

    Banco de dados: DQL do exercício 1.2 finalizado

--- Banco de dados/Exercícios/1.2 - Locadora/DQL.sql
+++ Banco de dados/Exercícios/1.2 - Locadora/DQL.sql
@@ -1,26 +1,37 @@
 -- DQL - Data Query Language
 
+USE Exercicio_1_2;
 
 -- Listar todos os alugueis mostrando as datas de início e fim, o nome do cliente que alugou e nome do modelo do carro
 SELECT
-	distinct
-	A.DataInicio,
-	A.DataFim,
-	C.Nome,
-	M.Nome,
-	A.IdAluguel
-FROM
-	Empresa AS E,
-	Cliente AS C,
-	Modelo AS M,
-	Marca AS Ma,
-	Veiculo AS V,
-	Aluguel AS A
-WHERE
-	A.IdCliente = C.IdCliente
-	AND A.IdVeiculo = V.IdVeiculo
-	AND M.IdModelo = V.IdModelo 
-	AND V.IdMarca = Ma.IdMarca
-
+	Aluguel.DataInicio,
+	Aluguel.DataFim,
+	Cliente.Nome,
+	Modelo.Nome
+FROM Veiculo
+INNER JOIN Aluguel ON Aluguel.IdVeiculo = Veiculo.IdVeiculo
+INNER JOIN Cliente ON Aluguel.IdCliente = Cliente.IdCliente
+INNER JOIN Modelo ON Modelo.IdModelo = Veiculo.IdModelo
 
 -- Listar os alugueis de um determinado cliente mostrando seu nome, as datas de início e fim e o nome do modelo do carro
+SELECT
+	Aluguel.DataInicio,
+	Aluguel.DataFim,
+	Cliente.Nome,
+	Modelo.Nome
+FROM Cliente
+INNER JOIN Aluguel ON Aluguel.IdCliente = Aluguel.IdCliente
+INNER JOIN Veiculo ON Veiculo.IdVeiculo = Aluguel.IdVeiculo
+INNER JOIN Modelo ON Modelo.IdModelo = Veiculo.IdModelo
+
+SELECT
+	Aluguel.DataInicio,
+	Aluguel.DataFim,
+	Cliente.Nome,
+	Modelo.Nome
+FROM Veiculo
+	INNER JOIN Aluguel ON Aluguel.IdVeiculo = Veiculo.IdVeiculo
+	INNER JOIN Cliente ON Aluguel.IdCliente = Cliente.IdCliente
+	INNER JOIN Modelo ON Modelo.IdModelo = Veiculo.IdModelo
+WHERE
+	Cliente.Nome LIKE 'Eduardo'
