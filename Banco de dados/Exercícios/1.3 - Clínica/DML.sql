4232b446424fe68bd6085832cbe79a78dca60a68
Gustavo-daCosta <80484719+Gustavo-daCosta@users.noreply.github.com>
2023-08-05 17:23:07 -0300

    Banco de dados: Exercícios 1.3 e 1.4 modificados

--- Banco de dados/Exercícios/1.3 - Clínica/DML.sql
+++ Banco de dados/Exercícios/1.3 - Clínica/DML.sql
@@ -4,8 +4,8 @@
 USE Exercicio_1_3;
 
 -- Inserir dados na tabela
-INSERT INTO Clinica(Endereco)
-VALUES ('Rua Niterói'), ('Avenida Brasil');
+INSERT INTO Clinica(Nome, Endereco)
+VALUES ('PetCare', 'Rua Niterói'), ('Hospital Pet', 'Avenida Brasil');
 
 INSERT INTO Veterinario(IdClinica, Nome, CRMV)
 VALUES (1, 'Carlos', '123456'),(2, 'Eduardo', '654321');
@@ -22,7 +22,10 @@ VALUES ('Gabriel'), ('Gustavo'), ('Guilherme')
 INSERT INTO Pet(IdTipoPet, IdRaca, IdDono, Nome, DataNascimento)
 VALUES (1, 1, 1, 'Tobi', '2021-10-31'), (1, 2, 2, 'Orion', '2016-04-20')
 
-INSERT INTO Atendimentos(IdVeterinario, IdPet)
-VALUES (1, 2), (1, 2)
+INSERT INTO Atendimentos(IdClinica, IdVeterinario, IdPet)
+VALUES (2, 1, 2), (1, 1, 2)
 
-SELECT * FROM Pet;
+-- Comandos para deletar todos os valores de todas as colunas de todas as tabelas
+EXEC sys.sp_msforeachtable 'ALTER TABLE ? NOCHECK CONSTRAINT ALL'
+EXEC sys.sp_msforeachtable 'DELETE FROM ?'
+EXEC sys.sp_msforeachtable 'ALTER TABLE ? CHECK CONSTRAINT ALL'
