2620b793de8f512a116a6ece4b6c54eb140bd5f8
Gustavo-daCosta <gustavo.costa70@aluno.senai.br>
2023-08-21 17:22:42 -0300

    API: Projeto inicial de filmes adicionado

---
+++ API/webapi.filmes.tarde/Domains/FilmeDomain.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webapi.filmes.tarde.Domains
+{
+    /// <summary>
+    /// Classe que representa a entidade(tabela) Filme
+    /// </summary>
+    public class FilmeDomain
+    {
+        public int IdFilme { get; set; }
+        [Required(ErrorMessage = "É obrigatória a escolha de um gênero para o filme")]
+        public int IdGenero { get; set; }
+        public GeneroDomain? Genero { get; set; }
+
+        [Required(ErrorMessage = "O tiítulo do filme é obrigatório")]
+        [StringLength(50)]
+        public string? Titulo { get; set; }
+    }
+}
