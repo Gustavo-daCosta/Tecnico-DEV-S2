46a4c141283c7aa99dc46c9e995b042b0b29de9f
Gustavo-daCosta <gustavo.costa70@aluno.senai.br>
2023-08-22 17:21:11 -0300

    API: Início dos controladores e conexões da API

---
+++ API/webapi.filmes.tarde/Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapi.filmes.tarde.Controllers
+{
+    /// <summary>
+    /// Define que a rota de uma requisição será no seguinte formato
+    /// dominio/api/nomeController
+    /// exemplo: http://localhost:5000/api/Genero
+    /// </summary>
+    [Route("api/[controller]")]
+    /// <summary>
+    /// Define quem é o controlador da API
+    /// </summary>
+    [ApiController]
+    /// <summary>
+    /// Define qual é o tipo de resposta da API
+    /// </summary>
+    [Produces("application/json")]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
--- API/webapi.filmes.tarde/Repositories/GeneroRepository.cs
+++ API/webapi.filmes.tarde/Repositories/GeneroRepository.cs
@@ -1,10 +1,20 @@
-﻿using webapi.filmes.tarde.Domains;
+﻿using System.Data.SqlClient;
+using webapi.filmes.tarde.Domains;
 using webapi.filmes.tarde.Interfaces;
 
 namespace webapi.filmes.tarde.Repositories
 {
     public class GeneroRepository : IGeneroRepository
     {
+        /// <summary>
+        /// String de conexão com o banco de dados que recebe os seguintes parâmetros:
+        /// Data Source: Nome do servidor do banco
+        /// Initial Catalog: Nome do banco de dados
+        /// Autenticação:
+        ///     - Windows: Integrated Security = True
+        ///     - SqlServer: User Id = sa; Pwd = Senha
+        /// </summary>
+        private string StringConexao = "Data Source = NOTE11-S14; Initial Catalog = Filmes_Tarde; User Id = sa; Pwd = Senai@134";
         public void AtualizarIdCorpo(GeneroDomain genero)
         {
             throw new NotImplementedException();
@@ -30,9 +40,47 @@ namespace webapi.filmes.tarde.Repositories
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Listar todos os objetos do tipo Gênero
+        /// </summary>
+        /// <returns>Lista de objetos do tipo Gênero</returns>
         public List<GeneroDomain> ListarTodos()
         {
-            throw new NotImplementedException();
+            // Cria uma lista de gêneros onde os gêneros serão armazenados
+            List<GeneroDomain> listaGeneros = new List<GeneroDomain>();
+
+            // Declara a SqlConnection passando a string de conexão como parâmetro
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                // Declara a instrução a ser executada
+                string querySelectAll = "SELECT IdGenero, Nome FROM Genero";
+
+                // Abre a conexão com o banco de dados
+                con.Open();
+
+                // Declara o SqlDataReader para percorrer(ler) a tabela no banco de dados
+                SqlDataReader rdr;
+
+                // Declara o SqlCommand passando a query que será executada e a conexão
+                using (SqlCommand cmd = new SqlCommand(querySelectAll, con))
+                {
+                    // Executa a query e armazena os dados no rdr
+                    rdr = cmd.ExecuteReader();
+
+                    // Enquanto houver registros para serem lidos no rdr, o laço se repetirá
+                    while (rdr.Read())
+                    {
+                        GeneroDomain genero = new GeneroDomain()
+                        {
+                            // Atribui a propriedade IdGenero o valor da primeira coluna da tabela
+                            IdGenero = Convert.ToInt32(rdr[0]),
+                            Nome = rdr["Nome"].ToString(),
+                        };
+                        listaGeneros.Add(genero);
+                    }
+                }
+            }
+            return listaGeneros;
         }
     }
 }
