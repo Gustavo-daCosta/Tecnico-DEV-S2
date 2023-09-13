fda7074e4f8b98bf0cebdf555dc1b56adf3d1256
Gustavo-daCosta <gustavo.costa70@aluno.senai.br>
2023-09-13 16:28:40 -0300

    API: Web API Database first concluida

--- API/webapi.inlock.tarde/Controllers/EstudioController.cs
+++ API/webapi.inlock.tarde/Controllers/EstudioController.cs
@@ -14,11 +14,88 @@ namespace webapi.inlock.tarde.Controllers
         private IEstudioRepository _estudioRepository = new EstudioRepository();
 
         [HttpGet]
+        [Route("ListarTodos")]
         public IActionResult Get()
         {
             try
             {
                 return Ok(_estudioRepository.Listar());
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("ListarComJogos")]
+        public IActionResult GetWithGames()
+        {
+            try
+            {
+                return Ok(_estudioRepository.ListarComJogos());
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("Deletar")]
+        public IActionResult Delete(Guid id)
+        {
+            try
+            {
+                _estudioRepository.Deletar(id);
+
+                return NoContent();
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("Cadastrar")]
+        public IActionResult Post(Estudio estudio)
+        {
+            try
+            {
+                _estudioRepository.Cadastrar(estudio);
+
+                return StatusCode(201);
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("BuscarPorId")]
+        public IActionResult GetById(Guid id)
+        {
+            try
+            {
+                return Ok(_estudioRepository.BuscarPorId(id));
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("Atualizar")]
+        public IActionResult Put(Guid id, Estudio estudio)
+        {
+            try
+            {
+                _estudioRepository.Atualizar(id, estudio);
+
+                return StatusCode(201);
             }
             catch (Exception error)
             {
--- API/webapi.inlock.tarde/Repositories/EstudioRepository.cs
+++ API/webapi.inlock.tarde/Repositories/EstudioRepository.cs
@@ -11,30 +11,37 @@ namespace webapi.inlock.tarde.Repositories
 
         public void Atualizar(Guid id, Estudio estudio)
         {
-            throw new NotImplementedException();
+            Estudio estudioBuscado = BuscarPorId(id);
+
+            if (estudioBuscado != null)
+            {
+                estudioBuscado.Nome = estudio.Nome;
+            }
+            ctx.Estudios.Update(estudioBuscado!);
+            ctx.SaveChanges();
         }
 
-        public Estudio BuscarPorId(Guid id)
-        {
-            throw new NotImplementedException();
-        }
+        public Estudio BuscarPorId(Guid id) => ListarComJogos().FirstOrDefault(estudio => estudio.IdEstudio == id)!;
 
         public void Cadastrar(Estudio estudio)
         {
-            throw new NotImplementedException();
+            ctx.Estudios.Add(estudio);
+            ctx.SaveChanges();
         }
 
         public void Deletar(Guid id)
         {
-            throw new NotImplementedException();
+            Estudio estudioBuscado = BuscarPorId(id)!;
+
+            if (estudioBuscado != null)
+            {
+                ctx.Estudios.Remove(estudioBuscado);
+                ctx.SaveChanges();
+            }
         }
 
         public List<Estudio> Listar() => ctx.Estudios.ToList();
 
-        public List<Estudio> ListarComJogos()
-        {
-            List<Jogo> jogos = ctx.Jogos.ToList();
-            return ctx.Estudios.Include(estudio => estudio.Jogos == jogos);
-        }
+        public List<Estudio> ListarComJogos() => ctx.Estudios.Include(estudio => estudio.Jogos).ToList();
     }
 }
